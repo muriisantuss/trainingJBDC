79bc66adb3f4869406c7f750e7e5768da69a669c
muriisantuss <muriisantuss@gmail.com>
2024-10-21 17:07:23 -0300

    :sparkles: feat(addLoja) package appLoja

---
+++ src/appLoja/appLoja.java
@@ -0,0 +1,11 @@
+package appLoja;
+
+import conexao.Conexao;
+
+public class appLoja {
+    public static void main(String[] args) throws Exception {
+        Conexao c = new Conexao();
+        c.getConexao();
+
+    }
+}
